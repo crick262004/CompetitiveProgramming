4284f377a1c77d74db6c0fc1570015803041c8e3
crick262004 <arnavg2644@gmail.com>
2026-04-12 03:13:31 +0530

    chore: code formatting

--- Linear Algebra/Matrix Multiplication.cpp
+++ Linear Algebra/Matrix Multiplication.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 const int mod = 1e9 + 7;
-#define REP(i) for(int i = 0; i < 2; ++i)
+#define REP(i) for (int i = 0; i < 2; ++i)
 
-struct Matrix {
-    int a[2][2] = {{0,0},{0,0}};
-    
-    Matrix operator *(const Matrix& other) const {
+struct Matrix
+{
+    int a[2][2] = {{0, 0}, {0, 0}};
+
+    Matrix operator*(const Matrix &other) const
+    {
         Matrix product;
-        REP(i) REP(j) REP(k) {
-            product.a[i][k] = (product.a[i][k] + (long long) a[i][j] * other.a[j][k]) % mod;
+        REP(i)
+        REP(j) REP(k)
+        {
+            product.a[i][k] = (product.a[i][k] + (long long)a[i][j] * other.a[j][k]) % mod;
         }
         return product;
     }
@@ -18,16 +22,19 @@ struct Matrix {
 
 // Seg Tree involving Matrix Multiplication
 
-struct node {
+struct node
+{
     Matrix nm;
 };
 
-struct SegTree {
+struct SegTree
+{
     int SZ;
     vector<node> t;
-    SegTree(int size) : SZ(size), t(4*size){}
-    
-    node merge(node a, node b) {
+    SegTree(int size) : SZ(size), t(4 * size) {}
+
+    node merge(node a, node b)
+    {
         // IMPORTANT : THE ORDER IS IMPORTANT!!!!!
         // doing ans = a * b is wrong.
         // correct is to do ans = b * a
@@ -37,40 +44,48 @@ struct SegTree {
         return ans;
     }
 
-    void build(int id, int l, int r, string& s) {   
-        if(l == r){
+    void build(int id, int l, int r, string &s)
+    {
+        if (l == r)
+        {
             // leaf node
             t[id].nm.init(s[l]); // acc. to qn
             return;
         }
-        int mid = (l + r)/2;
-        build(2*id, l ,mid, s);
-        build(2*id+1, mid + 1 ,r, s);
-        t[id] = merge(t[2*id], t[2*id + 1]);
+        int mid = (l + r) / 2;
+        build(2 * id, l, mid, s);
+        build(2 * id + 1, mid + 1, r, s);
+        t[id] = merge(t[2 * id], t[2 * id + 1]);
     }
 
-    void update(int id, int l, int r, int pos, node val) {
-        if((pos < l)|| (pos > r)){
-            return; 
-        } 
-        if( l == r){
+    void update(int id, int l, int r, int pos, node val)
+    {
+        if ((pos < l) || (pos > r))
+        {
+            return;
+        }
+        if (l == r)
+        {
             t[id] = val; // acc. to qn
             return;
         }
-        int mid = (l + r)/2;
-        update(2*id, l ,mid, pos, val);
-        update(2*id+1, mid + 1 ,r, pos , val);
-        t[id] = merge(t[2*id], t[2*id + 1]);
+        int mid = (l + r) / 2;
+        update(2 * id, l, mid, pos, val);
+        update(2 * id + 1, mid + 1, r, pos, val);
+        t[id] = merge(t[2 * id], t[2 * id + 1]);
     }
 
-    node query(int id, int l, int r, int lq, int rq) {
-        if((rq < l) || (lq > r)){
-            return node();  // acc. to qn
-        }   
-        if((lq <= l) && (r <= rq)){
-            return t[id];  // acc. to qn
+    node query(int id, int l, int r, int lq, int rq)
+    {
+        if ((rq < l) || (lq > r))
+        {
+            return node(); // acc. to qn
         }
-        int mid = (l + r)/2;
-        return merge(query(2*id, l, mid, lq, rq), query(2*id + 1, mid + 1, r, lq, rq));
+        if ((lq <= l) && (r <= rq))
+        {
+            return t[id]; // acc. to qn
+        }
+        int mid = (l + r) / 2;
+        return merge(query(2 * id, l, mid, lq, rq), query(2 * id + 1, mid + 1, r, lq, rq));
     }
 };
