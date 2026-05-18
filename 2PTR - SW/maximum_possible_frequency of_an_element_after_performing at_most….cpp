4b28af1741ae5ea17ed806f753fcc034e7efaa66
crick262004 <arnavg2644@gmail.com>
2026-05-18 08:12:01 +0530

    Clean up and add 2PTR - SW notes

---
+++ 2PTR - SW/maximum_possible_frequency of_an_element_after_performing at_most….cpp
@@ -0,0 +1,35 @@
+// maximum possible frequency of an element after performing at most…
+
+// maximum possible frequency
+
+// of an element after performing
+
+     at most
+   
+// k
+   
+// increments
+
+// .
+
+int maxFrequency(vector<int>& nums, int k) {
+    // 2 pointer sliding window
+
+        sort(nums.begin(), nums.end());        
+        int l = 0;
+        int r = 0;
+
+        int windowlen = 0;
+        long windowsum = 0;
+        while(r<nums.size())
+        {
+            windowsum += nums[r];
+            while( (long)nums[r]*(r-l+1) > windowsum + k )
+            {
+                windowsum -= nums[l++];
+            }
+            windowlen = max(windowlen, r-l+1);
+            r++;
+        }
+        return windowlen;
+    }
