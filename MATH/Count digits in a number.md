dc61a476c70205ca7bcd86a6c20d5a1e5891f213
Arnav Gupta <131391644+crick262004@users.noreply.github.com>
2026-06-05 01:23:58 +0530

    Update and rename Count digits in a number.md to count_digits.cpp

--- MATH/Count digits in a number.md
+++
@@ -1,7 +0,0 @@
-# Count digits in a number
-
-
- <span style="font-size: 30.0;text-align: left;">
-     int cnt = (int)(log10(n)+1);
-int k = to_string(num).length();
- </span>
---
+++ MATH/count_digits.cpp
@@ -0,0 +1,7 @@
+// Count digits in a number
+
+// Approach 1: Mathematical O(1) approach using logarithms
+int cnt = (int)(log10(n) + 1);
+
+// Approach 2: String conversion approach
+int k = to_string(num).length();
