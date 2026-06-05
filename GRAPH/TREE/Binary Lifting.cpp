e36a9a5ff37869107baca409d5546e8879baf30d
Arnav Gupta <131391644+crick262004@users.noreply.github.com>
2026-06-05 11:14:37 +0530

    Update and rename Binary Lifting.md to Binary Lifting.cpp

--- GRAPH/TREE/Binary Lifting.cpp
+++ GRAPH/TREE/Binary Lifting.cpp
@@ -1,13 +1,6 @@
-# Binary Lifting  (If parents array isn’t available, we can either first…
-
- <span style="font-size: 31.0;">
-     # **Binary Lifting 
-(If parents array isn’t available, we can either first calc. it, or just go in dfs order)**
-
- </span> <span style="font-size: 31.0;">
-     **1.**
-
- </span>class TreeAncestor {
+// (If parents array isn’t available, we can either first calc. it, or just go in dfs order)
+//1.
+class TreeAncestor {
 public:
     static const int m = 6e4;
     int lg[m+1];
@@ -29,7 +22,7 @@ public:
                 }
                 else
                     jumpind[jump][i] = jumpind[jump-1][jumpind[jump-1][i]];
-            }
+                }
         }
     }
     
@@ -53,13 +46,12 @@ public:
  * TreeAncestor* obj = new TreeAncestor(n, parent);
  * int param_1 = obj->getKthAncestor(node,k);
  */
- <span style="font-size: 31.0;">
-     
-**2.**
 
- </span>*const* int N = 2e5 + 5; 
-*vi* adjL[N];
-*vi* par(N);
+
+//2.
+const int N = 2e5 + 5; 
+vi adjL[N];
+vi par(N);
 ll n, q;
 void solve(){
     int K = 31;
@@ -71,7 +63,7 @@ void solve(){
     f(i,n){
         cin >> par[i+1];
     }
-    *vvi* jumpind(K+1, *vi*(n+1, -1));
+    vvi jumpind(K+1, vi(n+1, -1));
     for(int i = n;i>=1; i--){
         jumpind[0][i] = par[i];
     }
@@ -91,46 +83,45 @@ void solve(){
         }
         cout << ind << endl;
     }
-} <span style="font-size: 31.0;">
+} 
      
 
-**3.**
+//3.
+int lg[m+1];
+lg[1] = 0;
+for (int i = 2; i <= m; i++)
+    lg[i] = lg[i/2] + 1;
+int K = lg[m];
 
- </span>int lg[m+1];
-    lg[1] = 0;
-    for (int i = 2; i <= m; i++)
-        lg[i] = lg[i/2] + 1;
-    int K = lg[m];
-
-    *vvi* jumpind(K+1, *vi*(m, m));
-    for(int i = m-1;i>=0; i--){
-        jumpind[0][i] = last_ind[next_val[a[i]]];
-        last_ind[a[i]] = i;
+vvi jumpind(K+1, vi(m, m));
+for(int i = m-1;i>=0; i--){
+    jumpind[0][i] = last_ind[next_val[a[i]]];
+    last_ind[a[i]] = i;
+}
+for(int jump = 1; jump<=K; jump++){
+    for(int i = 0; i<m; i++){
+        if(jumpind[jump-1][i] == m){
+            jumpind[jump][i] = m;
+        }
+        else
+            jumpind[jump][i] = jumpind[jump-1][jumpind[jump-1][i]];
     }
-    for(int jump = 1; jump<=K; jump++){
-        for(int i = 0; i<m; i++){
-            if(jumpind[jump-1][i] == m){
-                jumpind[jump][i] = m;
-            }
-            else
-                jumpind[jump][i] = jumpind[jump-1][jumpind[jump-1][i]];
-        }
+}
+
+while(d){
+    int j = log2(d);
+    d -= (1<<j);
+    ind = jumpind[j][ind];
+    if(ind >= m){
+        flag = true;
+        break;
     }
+}
 
-       while(d){
-            int j = log2(d);
-            d -= (1<<j);
-            ind = jumpind[j][ind];
-            if(ind >= m){
-                flag = true;
-                break;
-            }
-        }
-OR 
-        for(int jump = 31; jump >= 0; jump--){
-            if(d >= (1ll<<jump)){
-                ind = jumpind[jump][ind];
-                d -= (1ll<<jump);
-            }   
-        }
-
+// OR 
+for(int jump = 31; jump >= 0; jump--){
+    if(d >= (1ll<<jump)){
+        ind = jumpind[jump][ind];
+        d -= (1ll<<jump);
+    }   
+}
