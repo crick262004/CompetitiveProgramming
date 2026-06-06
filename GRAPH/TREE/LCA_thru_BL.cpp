5a2e01a1615f8dca7b9bf43fad63025a4e19598e
Arnav Gupta <131391644+crick262004@users.noreply.github.com>
2026-06-06 13:06:11 +0530

    Update and rename LCA ( through Binary Lifting).md to LCA_thru_BL.cpp

--- GRAPH/TREE/LCA_thru_BL.cpp
+++ GRAPH/TREE/LCA_thru_BL.cpp
@@ -1,13 +1,14 @@
-# LCA ( through Binary Lifting)
+// LCA ( through Binary Lifting)
 
-*// solving:*
-*const* int N = 2e5 + 5;
-*vi* adjL[N];
-*vi* par(N, -1);
-*vi* depth(N, -1);
+// solving:
+const int N = 2e5 + 5;
+vi adjL[N];
+vi par(N, -1);
+vi depth(N, -1);
 int lg[N+1];
 ll n, m, q;
-**void dfs(int node, int p, int d){
+
+void dfs(int node, int p, int d){
     depth[node] = d;
     par[node] = p;
     for(auto v : adjL[node]){
@@ -16,15 +17,16 @@ ll n, m, q;
         }
     }
 }
-ll getLCA(ll a, ll b ,** ***vvi&*** **jumpind){
+
+ll getLCA(ll a, ll b, vvi& jumpind){
     if(depth[a] < depth[b])
-        swap(a, b);**
-    ***// a is the higher node always***
+        swap(a, b);
+    // a is the higher node always
 
-    **ll d = depth[a] - depth[b];**
+    ll d = depth[a] - depth[b];
 
-    ***// binary lifting to same level***
-    **for(int jump = lg[N]; jump >= 0; jump--){
+    // binary lifting to same level
+    for(int jump = lg[N]; jump >= 0; jump--){
         if(d >= (1<<jump)){
             a = jumpind[jump][a];
             d -= (1<<jump);
@@ -40,43 +42,44 @@ ll getLCA(ll a, ll b ,** ***vvi&*** **jumpind){
         }
     }
     return jumpind[0][a];
-}**
+}
+
 void solve(){
-    *// 1-based indexing* 
+    // 1-based indexing 
 
     cin >> n >> q;
 
-    *// cleaning*
+    // cleaning
     f(i,n+1){
         adjL[i].clear();
     }
     par.assign(n+1, -1);
     depth.assign(n+1, -1);
 
-    *// input 2 ways*
-    *// f(i,n-1){*
-    *//     ll u, v; cin >> u >> v;*
-    *//     adjL[u].pb(v);*
-    *//     adjL[v].pb(u);*
-    *// }*
+    // input 2 ways
+    // f(i,n-1){
+    //      ll u, v; cin >> u >> v;
+    //      adjL[u].pb(v);
+    //      adjL[v].pb(u);
+    // }
     f(i,n-1){
         cin >> par[i+2];
         adjL[par[i+2]].pb(i+2);
     }
 
-    *// preprocessing dfs ( calculate parents , depth)*
-    dfs(1, -1, 0); *// 1 is considered root*
+    // preprocessing dfs ( calculate parents , depth)
+    dfs(1, -1, 0); // 1 is considered root
 
-    *// baaki preprocessing*
+    // baaki preprocessing
     int root = 1;
     lg[1] = 0;
     for (int i = 2; i <= N; i++)
         lg[i] = lg[i/2] + 1;
     int K = lg[N];
-    *vvi* jumpind(K+1, *vi*(N, -1));
+    vvi jumpind(K+1, vi(N, -1));
 
-    ***// populating the jumps array***
-    **for(int i = N-1;i>0; i--){
+    // populating the jumps array
+    for(int i = N-1;i>0; i--){
         jumpind[0][i] = par[i];
     }
     for(int jump = 1; jump<=K; jump++){
@@ -92,10 +95,8 @@ void solve(){
     while(q--){
         int a, b; cin >> a >> b;
         cout << getLCA(a, b, jumpind) << endl;
-    }**
+    }
 }
 
- <span style="font-size: 33.0;">
-     
-**If qn involves thinking about the path bw 2 nodes in a tree, its built on LCA. Root the Tree at a node, find the LCA of node a and node b. Now we know the path from a to b.**
- </span>
+// If qn involves thinking about the path bw 2 nodes in a tree, its built on LCA. 
+// Root the Tree at a node, find the LCA of node a and node b. Now we know the path from a to b.
