f6985bf7743eea9e2a01810b1d2ec035661af30b
Arnav Gupta <131391644+crick262004@users.noreply.github.com>
2026-06-20 01:44:51 +0530

    Update permutations_ByRecursion.cpp

--- RECURSION/permutations_ByRecursion.cpp
+++ RECURSION/permutations_ByRecursion.cpp
@@ -30,3 +30,59 @@ public:
         return ans;
     }
 };
+
+
+// THE NEETCODE ITERATIVE SOLUTION.
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        // initializing the very first permutations list, 
+        // make sure to give it a empty permutation. Or else base case never hits.
+        // remember, an empty permutation is different from no permutation.
+        vector<vector<int>> ans = {{}};
+
+
+        // for each new value, we create a new set of permuations
+        // using the permutations made from previous values.
+        for(int x : nums)
+        {
+            // the new set of permutations.
+            vector<vector<int>> temp;
+
+            // go through each of the old permutations. 
+            // We will make new permutations from each old one.
+            for(vector<int> permutation : ans)
+            {
+                // from each old permutation, we make a few new ones.
+                // same thing as above.
+                for(int i = 0; i<=permutation.size() ; i++)
+                {
+                    vector<int> newpermutation(permutation);
+
+                    newpermutation.insert(newpermutation.begin() + i, x);
+
+                    temp.push_back(newpermutation);
+                }
+            }
+            ans = temp;
+        }
+        return ans;
+    }
+};
+
+
+// other approach:
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        do{
+            vector<int> tmp(nums);
+            ans.push_back(tmp);
+        } while(next_permutation(nums.begin(), nums.end()));
+        return ans;
+    }
+};
+
